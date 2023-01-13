e82cf48e143a4e060399f86f2b76ac4d7326de82
Shipilov Dmitriy <shipilenok1@gmail.com>
2023-01-14 00:18:51 +0300

    Accorde core/views.py to PEP8

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
+
 def forbidden(request, exception):
-    return render(request, 'core/403csrf.html', {'path': request.path}, status=403)
+    return render(request, 'core/403csrf.html',
+                  {'path': request.path}, status=403)
