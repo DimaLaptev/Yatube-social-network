f2b90c824e392f3be32b035e7086a7dab2a77a6c
Shipilov Dmitriy <shipilenok1@gmail.com>
2023-01-25 15:10:48 +0300

    Add CustomUserModel with phone

--- yatube/users/models.py
+++ yatube/users/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    email = models.EmailField(unique=True)
+    phone = PhoneNumberField(unique = True, null = False, blank = False)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
